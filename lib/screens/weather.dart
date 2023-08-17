983de6fbd98df697426b888ea9dbaa521b01704c
aymanemhd <aymane.199820.mehdi@gmail.com>
2023-08-17 03:09:03 +0200

    add screens

---
+++ lib/screens/weather.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class WeatherApp extends StatefulWidget {
+  const WeatherApp({super.key});
+
+  @override
+  State<WeatherApp> createState() => _WeatherAppState();
+}
+
+class _WeatherAppState extends State<WeatherApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0x212121),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print("call btn");
+                  },
+                  child: Text("Get Data"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
