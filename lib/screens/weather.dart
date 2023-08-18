4c21639d72ecce081174a59123c63801c4995622
aymanemhd <aymane.199820.mehdi@gmail.com>
2023-08-18 04:18:54 +0200

    design UI

--- lib/screens/weather.dart
+++ lib/screens/weather.dart
@@ -14,13 +14,18 @@ class _WeatherAppState extends State<WeatherApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0x212121),
+      backgroundColor:  const Color(0x212121),
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Text("Current Weather", style: TextStyle(color: Colors.black,fontSize: 32, fontWeight: FontWeight.bold),),
+              SizedBox(height: 20.0,),
+              Icon(Icons.sunny, color: Colors.amber, size: 64,),
+              SizedBox(height: 20.0,),
+              Text("${weatherModel?.currentWeather["temperature"] != null ? weatherModel?.currentWeather["temperature"]:0}c", style: const TextStyle(color: Colors.black,fontSize: 32, fontWeight: FontWeight.bold),),
               ElevatedButton(
                   onPressed: () async {
                     print("call btn");
