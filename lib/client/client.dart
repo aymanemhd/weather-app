8b9e9038d48a081f8754851586821913b0536a1b
aymanemhd <aymane.199820.mehdi@gmail.com>
2023-08-18 03:30:52 +0200

    add model and class

---
+++ lib/client/client.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+import 'package:weatherapp/model/model.dart';
+
+class WeatherApiClient {
+  Future<WeatherModel> request() async {
+    String Url ="https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m";
+    Response response = await Dio().get(Url);
+    final parsData = jsonDecode(response.toString());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:weatherapp/screens/weather.dart';
 
 void main() {
